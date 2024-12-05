802131b167d8aaf6056610d6e5c361b8fce1d34e
vilgax <ken.vilgax@gmail.com>
2024-12-05 16:12:23 +0300

    downlload button

--- src/components/DownloadPDF.tsx
+++ src/components/DownloadPDF.tsx
@@ -13,35 +13,35 @@ interface Props {
 const Download: FC<Props> = ({ data, setData }) => {
   const debounced = useDebounce(data, 500)
 
-  // function handleInput(e: React.ChangeEvent<HTMLInputElement>) {
-  //   if (!e.target.files?.length) return
+  function handleInput(e: React.ChangeEvent<HTMLInputElement>) {
+    if (!e.target.files?.length) return
 
-  //   const file = e.target.files[0]
-  //   file
-  //     .text()
-  //     .then((str: string) => {
-  //       try {
-  //         if (!(str.startsWith('{') && str.endsWith('}'))) {
-  //           str = atob(str)
-  //         }
-  //         const d = JSON.parse(str)
-  //         const dParsed = TInvoice.parse(d)
-  //         console.info('parsed correctly')
-  //         setData(dParsed)
-  //       } catch (e) {
-  //         console.error(e)
-  //         return
-  //       }
-  //     })
-  //     .catch((err) => console.error(err))
-  // }
+    const file = e.target.files[0]
+    file
+      .text()
+      .then((str: string) => {
+        try {
+          if (!(str.startsWith('{') && str.endsWith('}'))) {
+            str = atob(str)
+          }
+          const d = JSON.parse(str)
+          const dParsed = TInvoice.parse(d)
+          console.info('parsed correctly')
+          setData(dParsed)
+        } catch (e) {
+          console.error(e)
+          return
+        }
+      })
+      .catch((err) => console.error(err))
+  }
 
-  // function handleSaveTemplate() {
-  //   const blob = new Blob([JSON.stringify(debounced)], {
-  //     type: 'text/plain;charset=utf-8',
-  //   })
-  //   FileSaver(blob, title + '.template')
-  // }
+  function handleSaveTemplate() {
+    const blob = new Blob([JSON.stringify(debounced)], {
+      type: 'text/plain;charset=utf-8',
+    })
+    FileSaver(blob, title + '.template')
+  }
 
   const title = data.invoiceTitle ? data.invoiceTitle.toLowerCase() : 'invoice'
   return (
@@ -56,18 +56,18 @@ const Download: FC<Props> = ({ data, setData }) => {
       ></PDFDownloadLink>
       <p>Save PDF</p>
 
-      {/* <button
+      <button
         onClick={handleSaveTemplate}
         aria-label="Save Template"
         title="Save Template"
         className="download-pdf__template_download mt-40"
       />
-      <p className="text-small">Save Template</p> */}
+      <p className="text-small">Save Template</p>
 
-      {/* <label className="download-pdf__template_upload">
+      <label className="download-pdf__template_upload">
         <input type="file" accept=".json,.template" onChange={handleInput} />
       </label>
-      <p className="text-small">Upload Template</p> */}
+      <p className="text-small">Upload Template</p>
     </div>
   )
 }
